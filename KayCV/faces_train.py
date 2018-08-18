b5f79a72837db2ab146ece964e182c1c329c937c
Amr M. Kayid <amrkayid2027@gmail.com>
2018-08-19 00:09:51 +0200

    Get images name from its root folder

--- KayCV/faces_train.py
+++ KayCV/faces_train.py
@@ -9,4 +9,5 @@ for root, dirs, files in os.walk(image_dir):
     for file in files:
         if file.endswith('png') or file.endswith('jpg'):
             path = os.path.join(root, file)
-            print(path)
+            label = os.path.basename(root).replace(' ', '_').lower()
+            print(label, path)
