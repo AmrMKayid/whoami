0137e963eae7c519c50817974653ff6380dc474f
Amr M. Kayid <amrkayid2027@gmail.com>
2018-08-19 01:20:43 +0200

    Add ids to labels

--- KayCV/faces_train.py
+++ KayCV/faces_train.py
@@ -1,6 +1,16 @@
 import os
+import cv2
+import numpy as np
+from PIL import Image
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+face_cascade = cv2.CascadeClassifier('cascades/data/haarcascade_frontalface_alt2.xml')
+
+x_train = []
+y_labels = []
+
+current_id = 0
+label_ids = {}
 
 # Loading images
 image_dir = os.path.join(BASE_DIR, "images")
@@ -11,3 +21,23 @@ for root, dirs, files in os.walk(image_dir):
             path = os.path.join(root, file)
             label = os.path.basename(root).replace(' ', '_').lower()
             print(label, path)
+            if label in label_ids:
+                pass
+            else:
+                label_ids[label] = current_id
+                current_id += 1
+
+            id_ = label_ids[label]
+            print(label_ids)
+            pil_image = Image.open(path).convert('L')  # grayscale
+            image_array = np.array(pil_image, 'uint8')
+            print(image_array)
+
+            faces = face_cascade.detectMultiScale(image_array, scaleFactor=1.5, minNeighbors=5)
+            for x, y, w, h in faces:
+                roi = image_array[y:y + h, x:x + w]
+                x_train.append(roi)
+                y_labels.append(id_)
+
+# print(y_labels)
+# print(x_train)
