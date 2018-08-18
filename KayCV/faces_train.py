8884b994c9425c73784bdcee3db4d17aecd397f4
Amr M. Kayid <amrkayid2027@gmail.com>
2018-08-19 00:06:36 +0200

    Loading images from dir

---
+++ KayCV/faces_train.py
@@ -0,0 +1,12 @@
+import os
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+
+# Loading images
+image_dir = os.path.join(BASE_DIR, "images")
+
+for root, dirs, files in os.walk(image_dir):
+    for file in files:
+        if file.endswith('png') or file.endswith('jpg'):
+            path = os.path.join(root, file)
+            print(path)
