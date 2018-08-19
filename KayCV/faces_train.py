97bae6e31fd9a34c8417672d9a855a18ebb115df
Amr M. Kayid <amrkayid2027@gmail.com>
2018-08-19 02:00:53 +0200

    Create recognizer

--- KayCV/faces_train.py
+++ KayCV/faces_train.py
@@ -6,6 +6,7 @@ from PIL import Image
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 face_cascade = cv2.CascadeClassifier('cascades/data/haarcascade_frontalface_alt2.xml')
+recognizer = cv2.face.LBPHFaceRecognizer_create()
 
 x_train = []
 y_labels = []
@@ -21,7 +22,8 @@ for root, dirs, files in os.walk(image_dir):
         if file.endswith('png') or file.endswith('jpg'):
             path = os.path.join(root, file)
             label = os.path.basename(root).replace(' ', '_').lower()
-            print(label, path)
+            # print(label, path)
+
             if label in label_ids:
                 pass
             else:
@@ -29,11 +31,13 @@ for root, dirs, files in os.walk(image_dir):
                 current_id += 1
 
             id_ = label_ids[label]
-            print(label_ids)
+            # print(label_ids)
             pil_image = Image.open(path).convert('L')  # grayscale
             image_array = np.array(pil_image, 'uint8')
-            print(image_array)
+            # print(image_array)
 
+            # Detecting Face's Region of Interest
+            # and append it to training set with it's corresponding label in y_labels
             faces = face_cascade.detectMultiScale(image_array, scaleFactor=1.5, minNeighbors=5)
             for x, y, w, h in faces:
                 roi = image_array[y:y + h, x:x + w]
@@ -43,5 +47,9 @@ for root, dirs, files in os.walk(image_dir):
 # print(y_labels)
 # print(x_train)
 
+# Using Pickle to Save Label IDs
 with open('labels.pickle', 'wb') as f:
     pickle.dump(label_ids, f)
+
+recognizer.train(x_train, np.array(y_labels))
+recognizer.save("recognizers/face-trainer.yml")
